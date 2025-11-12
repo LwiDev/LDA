ed154593b0a377a5cc26b19ed6d34ea78b8a32ba
Lwi <priixy@gmail.com>
2025-11-12 04:02:10 -0500

    [ADD] crud, init, sync, theme commands, envCheck.js, .ldarc.example && [UPDATE] preview command

--- bin/lda.js
+++ bin/lda.js
@@ -5,6 +5,7 @@ import { runInit } from "../lib/commands/init.js";
 import { runCrud } from "../lib/commands/crud.js";
 import { runTheme } from "../lib/commands/theme.js";
 import { runPreview } from "../lib/commands/preview.js";
+import { runSync } from "../lib/commands/sync.js";
 
 const program = new Command();
 
@@ -15,11 +16,11 @@ program
 
 program
   .command("init")
-  .description("Créer un nouveau projet")
+  .description("Crée un nouveau projet SvelteKit avec ou sans panneau d'administration")
   .action(runInit);
 
 program
-  .command("crud")
+  .command("crud <name>")
   .description("Générer une section CRUD complète")
   .action(runCrud);
 
@@ -31,6 +32,15 @@ program
 program
   .command("preview")
   .description("Lancer le mode vitrine / démo client")
+  .option("-p, --port <number>", "Port du serveur de démo", "5555")
   .action(runPreview);
+
+program
+  .command("sync")
+  .description("Synchronise le projet avec la dernière version de AdminTemplate")
+  .option("--force", "Forcer la mise à jour sans confirmation")
+  .option("--dry", "Simulation sans modification")
+  .option("--silent", "Mode silencieux")
+  .action(runSync);
 
 program.parse();
